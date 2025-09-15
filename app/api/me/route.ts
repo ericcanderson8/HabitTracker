637d9987306c8a8b84e571a808889818e3337f13
kyle_g <kgil204@outlook.com>
2025-09-15 14:15:16 -0700

    Previous session checking change added additional overhead, modified code to eliminate

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,15 +1,12 @@
 // app/api/me/route.ts (Next.js App Router)
 
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!; // only backend knows this
 
-export async function GET(req: Request) {
-  const userCookies = await cookies()
-  
-  const token = userCookies.get("token")?.value
+export async function GET(req: NextRequest) {
+  const token = req.cookies.get("token")?.value
 
   if (!token) {
     return NextResponse.json({ loggedIn: false }, { status: 401 });
